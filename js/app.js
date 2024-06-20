b00525dc9c5f0250980ba8f1085db992cb14a1a5
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-06-20 16:30:32 +0000

    concatenaçao

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 29;
+console.log(numeroSecreto)
 
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// condicional 
 if (numeroSecreto == chute) {
-    alert(`Isso ai! você acertou o número secreto! (${chute})`)
+    alert(`Isso ai! você acertou o número secreto! (${numeroSecreto})`)
 } else {
     alert('Você errou!')
 }
